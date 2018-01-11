8ef1f86af31bdd12c019c928bf8efc81f4537955
snehi <snehi@172.17.5.117>
2018-01-11 18:01:42 -0500

    SM Untested - This code is editing the Robot package: States the ports for the robot motors and joystick. This code also edits the commands and subsystem package: Arcade mode has been completed, along with the corresponding code in DriveTrain.

--- src/org/usfirst/frc/team4561/robot/OI.java
+++ src/org/usfirst/frc/team4561/robot/OI.java
@@ -7,11 +7,49 @@
 
 package org.usfirst.frc.team4561.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	private static Joystick leftStick = new Joystick (RobotMap.LEFT_JOYSTICK_PORT);
+	private static Joystick rightStick = new Joystick (RobotMap.RIGHT_JOYSTICK_PORT);
+
+	public double getRightStickY() {
+		
+		double rightStickY = rightStick.getY(); 
+		
+		return rightStickY;
+		
+	}
+	
+	public double getRightStickX() {
+		
+		double rightStickX = rightStick.getX(); 
+		
+		return rightStickX;
+		
+	}
+	
+	public double getLeftStickY() {
+		
+		double leftStickY = leftStick.getY(); 
+		
+		return leftStickY;
+		
+	}
+	
+public double getLeftStickX() {
+		
+		double leftStickX = leftStick.getX(); 
+		
+		return leftStickX;
+		
+	}
+	
+	
 	//// CREATING BUTTONS
 	// One type of button is a joystick button which is any button on a
 	//// joystick.
--- src/org/usfirst/frc/team4561/robot/RobotMap.java
+++ src/org/usfirst/frc/team4561/robot/RobotMap.java
@@ -13,11 +13,36 @@ package org.usfirst.frc.team4561.robot;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
+
+
+
 public class RobotMap {
+	
+	//Joystick Ports
+	public static final int LEFT_JOYSTICK_PORT = 0;
+	public static final int RIGHT_JOYSTICK_PORT = 1;
+	
 	// For example to map the left and right motors, you could define the
 	// following variables to use with your drivetrain subsystem.
 	// public static int leftMotor = 1;
 	// public static int rightMotor = 2;
+	
+	//Left Motor Ports
+	public static final int FRONT_LEFT_MOTOR_PORT = 9;
+	public static final int MID_LEFT_MOTOR_PORT = 5;
+	public static final int BACK_LEFT_MOTOR_PORT = 3;
+	
+	//Right Motor Ports
+	public static final int FRONT_RIGHT_MOTOR_PORT = 0;
+	public static final int MID_RIGHT_MOTOR_PORT = 4;
+	public static final int BACK_RIGHT_MOTOR_PORT = 10;
+	
+	//Transmission Motor Ports
+	public static final int TRANSMISSION_SPEED_BUTTON = 6;
+	public static final int TRANSMISSION_TORQUE_BUTTON = 4;
+	
+	//Drive Mode 
+	public static final int DRIVE_MODE = 0; // 1 is arcade drive, 0 is tank drive
 
 	// If you are using multiple modules, make sure to define both the port
 	// number and the module. For example you with a rangefinder:
