b27fcdca612e5989dc7259c2cfee371ddfd0da5d
BJSgeo <joc921cal@gmail.com>
2018-01-20 12:05:00 -0500

    BG - UNTESTED
    Initial automodes package, added automodes to cross the center line,
    added TankDriveTimed command

---
+++ src/org/usfirst/frc/team4561/robot/automodes/AutoDriveToLine.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team4561.robot.automodes;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import org.usfirst.frc.team4561.robot.commands.TorqueGear;
+import org.usfirst.frc.team4561.robot.commands.TankDriveTimed;
+/**
+ * This auto mode drives past the auto line if the robot is on the left or right side of the field.
+ * @author Ben
+ */
+public class AutoDriveToLine extends CommandGroup {
+	public AutoDriveToLine() {
+		// torque mode
+		addSequential(new TorqueGear());
+		// cross the auto line and score points
+		addSequential(new TankDriveTimed(1, 1, 2));
+		// addSequential(new WaitCommand(3));
+		// done
+	}
+}
---
+++ src/org/usfirst/frc/team4561/robot/automodes/AutoDriveToLineCenter.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team4561.robot.automodes;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import org.usfirst.frc.team4561.robot.commands.TorqueGear;
+import org.usfirst.frc.team4561.robot.commands.TankDriveTimed;
+/**
+ * This auto mode drives past the auto line if the robot is in the center of the field.
+ * @author Ben
+ */
+public class AutoDriveToLineCenter extends CommandGroup {
+	public AutoDriveToLineCenter() {
+		// torque mode
+		addSequential(new TorqueGear());
+		// forward
+		addSequential(new TankDriveTimed(1, 1, 1));
+		// turn right
+		addSequential(new TankDriveTimed(1, 0.5, 1));
+		// forward
+		addSequential(new TankDriveTimed(1, 1, 1));
+		// turn left
+		addSequential(new TankDriveTimed(0.5, 1, 1));
+		// cross the line and score points
+		addSequential(new TankDriveTimed(1, 1, 1));
+		// addSequential(new WaitCommand(3));
+		// done
+	}
+}
