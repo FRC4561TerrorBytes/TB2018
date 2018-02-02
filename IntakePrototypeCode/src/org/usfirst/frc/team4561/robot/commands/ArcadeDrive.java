37f82f0e273164a7589fe870b31331a17971145e
Lucas Therrien <ltherrien19@rthighschool.org>
2018-02-02 10:22:02 -0500

    LT Tested
    My intake prototype code

---
+++ IntakePrototypeCode/src/org/usfirst/frc/team4561/robot/commands/ArcadeDrive.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team4561.robot.commands;
+
+import org.usfirst.frc.team4561.robot.Robot;
+import org.usfirst.frc.team4561.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ArcadeDrive extends Command {
+	
+	public ArcadeDrive() {
+		requires(Robot.DriveTrain);
+	}
+	
+	protected void execute() {
+		if(RobotMap.TOURING_MODE == true){
+			Robot.DriveTrain.arcadeDrive(Robot.m_oi.getLeftStickX()*.5, Robot.m_oi.getRightStickY()*.5);
+		}
+		else{
+			Robot.DriveTrain.arcadeDrive(Robot.m_oi.getRightStickY(), Robot.m_oi.getLeftStickX());
+		}
+	}	  
+	  
+	protected void initialize() {}
+	  
+	protected void end() {}
+	  
+	protected void interrupted() {
+		end();
+	}
+    
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
