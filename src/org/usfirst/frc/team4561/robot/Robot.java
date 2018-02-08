cf0aad8472584819ed0dcbd9b378756ade82cca1
alekj <alekj@LAPTOP-IO7GV8K9>
2018-02-08 13:13:00 -0500

    AL - UNTESTED - Fixed robot type (TimedRobot into IterativeRobot)

--- src/org/usfirst/frc/team4561/robot/Robot.java
+++ src/org/usfirst/frc/team4561/robot/Robot.java
@@ -7,7 +7,7 @@
 
 package org.usfirst.frc.team4561.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -21,7 +21,7 @@ import org.usfirst.frc.team4561.robot.subsystems.*;
  * creating this project, you must also update the build.properties file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends IterativeRobot {
 	public static final DriveTrainPID driveTrain = new DriveTrainPID();
 	public static OI oi;
 	public static final ElevatorPID elevatorPID = new ElevatorPID();
