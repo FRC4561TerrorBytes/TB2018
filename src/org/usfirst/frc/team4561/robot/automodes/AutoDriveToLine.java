16d0a07d2a54d7259b4d4db271141f6bb6cc909e
alekj <alekj@LAPTOP-IO7GV8K9>
2018-02-15 16:37:09 -0500

    AL - UNTESTED - Updated Motion Magic, allowed for easier auto mode implementation

--- src/org/usfirst/frc/team4561/robot/automodes/AutoDriveToLine.java
+++ src/org/usfirst/frc/team4561/robot/automodes/AutoDriveToLine.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import org.usfirst.frc.team4561.robot.commands.TorqueGear;
 import org.usfirst.frc.team4561.robot.Robot;
+import org.usfirst.frc.team4561.robot.commands.DriveMagic;
 import org.usfirst.frc.team4561.robot.commands.TankDriveTimed;
 /**
  * This auto mode drives past the auto line if the robot is on the left or right side of the field.
@@ -14,7 +15,7 @@ public class AutoDriveToLine extends CommandGroup {
 		// torque mode
 		addSequential(new TorqueGear());
 		// cross the auto line and score points
-		addSequential(new TankDriveTimed(1, 1, 2));
+		addSequential(new DriveMagic(100, 100));
 		// addSequential(new WaitCommand(3));
 		// done
 	}
--- src/org/usfirst/frc/team4561/robot/commands/DriveMagic.java
+++ src/org/usfirst/frc/team4561/robot/commands/DriveMagic.java
@@ -7,19 +7,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveMagic extends Command {
 
+	int left;
+	int right;
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 	
-	public DriveMagic(){
+	public DriveMagic(int Left, int Right){
+		left = Left;
+		right = Right;
 		requires(Robot.driveTrain);
 	}
 	protected void execute(){
 		
-			double tLeft = 3600*Robot.oi.getLeftStickY()*50;
-			double tRight = -1800*Robot.oi.getLeftStickY()*50;
+			double tLeft = -3600*left;
+			double tRight = 1800*right;
 			Robot.driveTrain.magicDrive(tLeft, tRight);
 			System.out.print(tLeft);
 			System.out.print(" ");
