778315cf1c13caea04b281094ac274be51764d40
Lucas Therrien <ltherrien19@rthighschool.org>
2018-02-20 19:24:58 -0500

    LT UNTESTED:
    added my intake jig thing
    note intake left and right commands are now on 1/8 second timeouts

--- src/org/usfirst/frc/team4561/robot/commands/IntakeLeft.java
+++ src/org/usfirst/frc/team4561/robot/commands/IntakeLeft.java
@@ -17,7 +17,7 @@ public class IntakeLeft extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	//setTimeout(0.125);
+    	setTimeout(0.125);
     }
 
     // Called repeatedly when this Command is scheduled to run
