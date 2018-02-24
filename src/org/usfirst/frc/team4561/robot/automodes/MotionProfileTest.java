94ebb8d1cb5cba4629f6dde5529a00996a71b848
alekj <alekj@LAPTOP-IO7GV8K9>
2018-02-23 23:53:26 -0500

    AL - UNTESTED - Added command that waits until a certain percent of the motion profile has been covered.

--- src/org/usfirst/frc/team4561/robot/automodes/MotionProfileTest.java
+++ src/org/usfirst/frc/team4561/robot/automodes/MotionProfileTest.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team4561.robot.automodes;
 
 import org.usfirst.frc.team4561.robot.Robot;
 import org.usfirst.frc.team4561.robot.commands.DriveProfile;
+import org.usfirst.frc.team4561.robot.commands.WaitUntilPositionPercent;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import jaci.pathfinder.Trajectory;
@@ -10,7 +11,9 @@ public class MotionProfileTest extends CommandGroup {
 	public MotionProfileTest(){
 		Trajectory pointsR = Robot.midSwitchLeft.getRightTrajectory();
 		Trajectory pointsL = Robot.midSwitchLeft.getLeftTrajectory();
+		double start = pointsL.get(0).position;
+		double end = pointsL.get(pointsL.length()-1).position;
 		addSequential(new DriveProfile(pointsR, pointsL));
-		//addSequential(new WaitUntilPosition());
+		addSequential(new WaitUntilPositionPercent(0.5, start, end));
 	}
 }
---
+++ src/org/usfirst/frc/team4561/robot/commands/WaitUntilPositionPercent.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team4561.robot.commands;
+
+import org.usfirst.frc.team4561.robot.Robot;
+import org.usfirst.frc.team4561.robot.subsystems.DriveTrainPID;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class WaitUntilPositionPercent extends Command {
+
+	double goal;
+	double startPos;
+	double fullPos;
+	
+	public WaitUntilPositionPercent(double percent, double start, double end){
+		goal = percent;
+		startPos = start;
+		fullPos = end;
+	}
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return (Robot.driveTrain.getLeftPos()+startPos)/(DriveTrainPID.kInchesToTicks*12)>=goal*fullPos;
+	}
+	
+	protected void end(){
+		System.out.println("It happened");
+	}
+}
