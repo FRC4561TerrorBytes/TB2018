e53cc69bfa69c0e75b84a7291cb1dd44c538a6dd
alekj <alewis@rthighschool.org>
2018-04-05 09:16:20 -0400

    AL - UNTESTED - Reverted Scale Wheelbase

--- src/org/usfirst/frc/team4561/robot/RobotMap.java
+++ src/org/usfirst/frc/team4561/robot/RobotMap.java
@@ -126,7 +126,7 @@ public class RobotMap {
 	// Automode constraints
 	public static final double TIME_STEP = 0.05; // sec
 	public static final double WHEELBASE_WIDTH_SWITCH = 2.95; // 2.8;// 3.3;//3.1; //7; // ft: 1.865 for Delta (fudged fo 2.95), 1.375 for Kongo, 1.865 for Janderson
-	public static final double WHEELBASE_WIDTH_SCALE = 2.95;
+	public static final double WHEELBASE_WIDTH_SCALE = 2.8;
 	public static final double MAX_VELOCITY = 5; //ft/sec: 15.9 for Delta (decreased to 13.9) in speed gear
 	public static final double MAX_ACCELERATION = 1; // ft/s/s: 6 for Delta
 	public static final double MAX_JERK = 60.0; // ft/s/s/s
